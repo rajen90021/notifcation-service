d59b027b3545c769badb2272fb73f7f1acffa25e
rajengupta90021 <rajengupta@xetabots.com>
2026-01-11 21:03:34 +0530

    added ci

---
+++ src/config/kafka.ts
@@ -0,0 +1,80 @@
+import config from "config";
+import { Consumer, EachMessagePayload, Kafka, KafkaConfig } from "kafkajs";
+import { MessageBroker } from "../types/broker";
+import { createNotificationTransport } from "../factories/notification-factory";
+import { handleOrderHtml, handleOrderText } from "../handlers/orderHander";
+
+export class KafkaBroker implements MessageBroker {
+  private consumer: Consumer;
+
+  constructor(clientId: string, brokers: string[]) {
+    let kafkaConfig: KafkaConfig = {
+      clientId,
+      brokers,
+    };
+
+    if (process.env.NODE_ENV === "production") {
+      kafkaConfig = {
+        ...kafkaConfig,
+        ssl: true,
+        connectionTimeout: 45000,
+        sasl: {
+          mechanism: "plain",
+          username: config.get("kafka.sasl.username"),
+          password: config.get("kafka.sasl.password"),
+        },
+      };
+    }
+
+    const kafka = new Kafka(kafkaConfig);
+
+    this.consumer = kafka.consumer({ groupId: clientId });
+  }
+
+  /**
+   * Connect the consumer
+   */
+  async connectConsumer() {
+    await this.consumer.connect();
+  }
+
+  /**
+   * Disconnect the consumer
+   */
+  async disconnectConsumer() {
+    await this.consumer.disconnect();
+  }
+
+  async consumeMessage(topics: string[], fromBeginning: boolean = false) {
+    await this.consumer.subscribe({ topics, fromBeginning });
+
+    await this.consumer.run({
+      eachMessage: async ({
+        topic,
+        partition,
+        message,
+      }: EachMessagePayload) => {
+        // Logic to handle incoming messages.
+        console.log({
+          value: message.value.toString(),
+          topic,
+          partition,
+        });
+
+        if (topic === "order") {
+          //todo: Decide whether to send notification or not. // according to event_type.
+          const transport = createNotificationTransport("mail");
+
+          const order = JSON.parse(message.value.toString());
+
+          await transport.send({
+            to: order.data.customerId.email || config.get("mail.from"),
+            subject: "Order update.",
+            text: handleOrderText(order),
+            html: handleOrderHtml(order),
+          });
+        }
+      },
+    });
+  }
+}
